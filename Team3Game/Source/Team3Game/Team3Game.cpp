5483bef1e238d7195c0228c839caa9e82d9ef264
Kevin <1016452@student.sae.edu.au>
2020-07-24 13:21:05 +1000

    converted project to c++
     to make multiplayer work, starting on steam integration now

---
+++ Team3Game/Source/Team3Game/Team3Game.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Team3Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team3Game, "Team3Game" );
